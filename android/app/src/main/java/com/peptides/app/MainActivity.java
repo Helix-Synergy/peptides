c7e99d1c8c3b04a7bce8c4de0d40999cb9b1a595
Shashiyadav8 <vallalashashi9848@gmail.com>
2026-05-22 15:51:27 +0530

    Add Capacitor Android project and GitHub Actions build workflow

---
+++ android/app/src/main/java/com/peptides/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.peptides.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.peptides.app',
+  appName: 'Peptides',
+  webDir: 'build'
+};
+
+export default config;
